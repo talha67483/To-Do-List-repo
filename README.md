df9e649b2d4a3035d8cc50878a25b4e738bbfba4
talha67483 <mh2971966@gmail.com>
2024-08-30 09:59:57 +0500

    add file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Cli To-Do-List 
+<br>
+<h3> Use this command to run this To Do list in your System</h3>
+
+
+
---
+++ app.ts
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+
+let cond = true;
+
+while (cond) {
+  let addTodo = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: "What you want to add in your Todos",
+    },
+  ]);
+
+  if (addTodo.todo) {
+    todos.push(addTodo.todo);
+    console.log(todos);
+
+    let moreOptions = await inquirer.prompt([
+      {
+        name: "option",
+        type: "list",
+        message: "Enter one option from following",
+        choices: ["You wnat to add more Todos", "You want to remove Todos"],
+      },
+    ]);
+
+    if (moreOptions.option === "You wnat to add more Todos") {
+      let add = await inquirer.prompt([
+        {
+          name: "more",
+          type: "confirm",
+          message: "Do you want to add more Todos ?",
+          default: "false",
+        },
+      ]);
+
+      cond = add.more;
+      console.log(todos);
+    } else if (moreOptions.option === "You want to remove Todos") {
+      let removes = await inquirer.prompt([
+        {
+          name: "remove",
+          type: "input",
+          message: "Enter the item name to remove",
+        },
+      ]);
+      if (removes.remove === addTodo.todo) {
+        todos.pop();
+        console.log(todos);
+      }
+    }
+  } else {
+    console.log("Please enter something");
+  }
+}
